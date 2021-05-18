613d4768eccf8932642832e9d901eb7406a55898
Blomma <62100615+BlommaWalla@users.noreply.github.com>
2021-05-18 14:57:56 +0200

    stinky winky (character wrapper crashar)

--- Monke/Character.cs
+++ Monke/Character.cs
@@ -4,9 +4,10 @@ namespace Monke
 {
     public class Character
     {
-        public string name{get; set;}
-        public string gender{get; set;}
-
+        // public string name{get; set;}
+        // public string gender{get; set;}
+        public string name;
+        public string gender;
 
 
     }
